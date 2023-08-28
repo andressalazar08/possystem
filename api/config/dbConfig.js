6965c6d6daa974092c050a731556297a142aa7d9
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-08-28 14:08:37 -0500

    db config

---
+++ api/config/dbConfig.js
@@ -0,0 +1,44 @@
+const dotenv = require('dotenv').config()
+
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.DB,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.HOST,
+        dialect: 'mysql',
+        operatorsAliases:0,
+        pool:{
+            min:0,
+            max: 5,
+            acquire:30000,
+            idle:10000
+        
+        }
+    }
+)
+
+
+sequelize.authenticate()
+.then(()=>{
+    console.log('DB connected')
+})
+.catch(err=>{
+    console.log('Error on db ' +err)
+})
+
+
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.sequelize.sync({force:true})
+.then(()=>{
+    console.log('Resync on DB!')
+})
+
+
+module.exports = db;
--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}))
 
 //simple api test
 app.get('/', (req, res)=>{
