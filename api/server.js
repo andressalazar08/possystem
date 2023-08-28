4a83f698b035ef6187a482b1ac614958ece1bfe3
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-08-28 11:37:51 -0500

    initial config and installations

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+
+//simple api test
+app.get('/', (req, res)=>{
+    res.json({
+        message:"API pos system live"
+    })
+})
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log('Server is running on port: ' + PORT)
+})
