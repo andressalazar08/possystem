cd602abe4d3d791b10993dc4f8cea4606e77dd4c
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-08-29 11:42:12 -0500

    adduser controller and route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -13,6 +13,26 @@ const getUsers = async(req, res)=>{
 }
 
 
+const addUser = async (req, res)=>{
+    
+    const userFound = await User.findOne({where:{username:req.body.username}})
+
+    if(userFound ===null){
+        const user = await User.create(req.body)
+        res.status(200).json({
+            message:"User created successfully",
+            user
+        })
+    }else{
+        res.status(403).json({
+            message:"User already exists"
+        })
+
+    }
+  
+}
+
 module.exports ={
-    getUsers
+    getUsers,
+    addUser
 }
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -6,7 +6,9 @@ const router = require('express').Router();
 //Routes definition
 //1. use localhost:4000/api/getUsers
 router.get('/getUsers', userController.getUsers)
-
+//2. create a user 
+// use localhost:4000/api/addUser
+router.post('/addUser', userController.addUser)
 
 
 module.exports = router
